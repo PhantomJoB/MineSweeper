a75ea4f6f441d11e5db4ae1544c98bc11cf842b4
Jonathan <phantomjob@hotmail.com>
2015-11-02 18:51:23 -0500

    Create the project with Analyser UnitTests

---
+++ MinesweeperTools.UnitTest/AnalyserTest.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MinesweeperTools;
+
+namespace MinesweeperAnalyser.UnitTest
+{
+	[TestClass]
+	public class AnalyserTest
+	{
+		bool[,] _bombsMatrix;
+
+		[TestInitialize]
+		public void Initialize()
+		{
+			_bombsMatrix = CreateTestBombsMatrix();
+		}
+
+		[TestMethod]
+		public void FindBombs_WhenTwoArround_ReturnTwo()
+		{
+			Analyser analyser = new Analyser(_bombsMatrix);
+
+			int nbrBomb = analyser.FindBombs(2, 1);
+
+			Assert.AreEqual(2, nbrBomb);
+		}
+
+		[TestMethod]
+		public void FindBombs_WhenOneArround_ReturnOne()
+		{
+			Analyser analyser = new Analyser(_bombsMatrix);
+
+			int nbrBomb = analyser.FindBombs(0, 0);
+
+			Assert.AreEqual(1, nbrBomb);
+		}
+
+		[TestMethod]
+		public void FindBombs_WhenNoneArround_ReturnZero()
+		{
+			Analyser analyser = new Analyser(_bombsMatrix);
+
+			int nbrBomb = analyser.FindBombs(3, 0);
+
+			Assert.AreEqual(0, nbrBomb);
+		}
+
+		[TestMethod]
+		[ExpectedException(typeof(OutsideMatrixException))]
+		public void FindBombs_WhenOutsideOfMatrix_CoordinateX_ThrowException()
+		{
+			Analyser analyser = new Analyser(_bombsMatrix);
+
+			int nbrBomb = analyser.FindBombs(5, 0);
+		}
+
+		[TestMethod]
+		[ExpectedException(typeof(OutsideMatrixException))]
+		public void FindBombs_WhenOutsideOfMatrix_CoordinateY_ThrowException()
+		{
+			Analyser analyser = new Analyser(_bombsMatrix);
+
+			int nbrBomb = analyser.FindBombs(0, 5);
+		}
+
+		[TestMethod]
+		[ExpectedException(typeof(OutsideMatrixException))]
+		public void FindBombs_WhenOutsideOfMatrix_CoordinateXandY_ThrowException()
+		{
+			Analyser analyser = new Analyser(_bombsMatrix);
+
+			int nbrBomb = analyser.FindBombs(9, 9);
+		}
+
+		[TestMethod]
+		public void FindBombs_WhenOnABomb_ReturnMinusOne()
+		{
+			Analyser analyser = new Analyser(_bombsMatrix);
+
+			int nbrBomb = analyser.FindBombs(1, 1);
+
+			Assert.AreEqual(-1, nbrBomb);
+		}
+
+		bool[,] CreateTestBombsMatrix()
+		{
+			bool[,] bombsMatrix = new bool[4, 4] 
+			{
+				{false, false, false, false},
+				{false, false, false, false},
+				{false, false, true, false},
+				{false, false, false, false},
+			};
+
+			bombsMatrix[1, 1] = true;
+			bombsMatrix[2, 2] = true;
+			return bombsMatrix;
+		}
+	}
+}
---
+++ MinesweeperTools/Analyser.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinesweeperTools
+{
+    public class Analyser
+    {
+		public Analyser(bool[,] bombsMatrix)
+		{
+		}
+
+		// La premiere coordonné est 0, 0
+		public int FindBombs(int coordinateX, int coordinateY)
+		{
+			return 999;
+		}
+    }
+}
